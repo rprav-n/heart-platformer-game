7936c2e785bdd8c5c406625f5bacbe66248426c0
Praveen <rpraveen.engineer@gmail.com>
2023-10-28 10:20:32 +0530

    Added Player Movement Data as custom resource

---
+++ resources/player_movement_data.gd
@@ -0,0 +1,9 @@
+class_name PlayerMovementData
+
+extends Resource
+
+@export var speed: int = 90
+@export var acceleration: int = 800
+@export var friction: int = 1000
+@export var jump_speed: int = -270
+@export var air_resistance: int = 100
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,10 +2,7 @@ class_name Player
 
 extends CharacterBody2D
 
-const SPEED: int = 90
-const ACCELERATION: int = 800
-const FRICTION: int = 1000
-const JUMP_SPEED: int = -270
+@export var movement_data: PlayerMovementData
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -23,6 +20,7 @@ func _physics_process(delta: float):
 	
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
 	update_animation(input_axis)
 	
 	var was_on_floor: bool = is_on_floor()
@@ -42,21 +40,25 @@ func apply_gravity(delta: float):
 func handle_jump():
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_SPEED
+			velocity.y = movement_data.jump_speed
 	if not is_on_floor():
-		if Input.is_action_just_released("jump") and velocity.y < JUMP_SPEED / 2.0:
-			velocity.y = JUMP_SPEED / 2.0
+		if Input.is_action_just_released("jump") and velocity.y < movement_data.jump_speed / 2.0:
+			velocity.y = movement_data.jump_speed / 2.0
 
 
 func handle_acceleration(input_axis: float, delta: float):
 	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
 
 
 func apply_friction(input_axis: float, delta: float):
-	if input_axis == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
 
+
+func apply_air_resistance(input_axis: int, delta: float):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 
 func update_animation(input_axis: float):
 	if input_axis != 0:
